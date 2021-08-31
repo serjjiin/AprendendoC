41ba3475a869c732ce1e4055e3b461c967d561f3
serjin <slgrippa2@gmail.com>
2021-08-31 16:21:29 -0300

    Update c.c

--- LinguagemC/c.c
+++ LinguagemC/c.c
@@ -1,5 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    printf("ola mundo")
+int main(void)
+{
+
+    int num;
+
+    printf("Informe um número para verificar se é par ou impar: ");
+    scanf ("%i",&num);
+    
+    if (num % 2 == 0){
+        printf ("O número %i é par!!",num);
+    }else {
+        printf("O número %i é impar!!",num);
+    }
+
+    printf("\nVerificador desligando...");
 }
+
+// usei um compilador online msm ashuashuashu
